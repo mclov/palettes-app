ec5d06d3c4160a6dbf97f9dfe772d144d28d6d38
Michael Clovis <michaelclovis@protonmail.com>
2019-12-11 11:00:50 -0800

    Add palette

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Palette from './Palette';
+import seedColors from './seedColors';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Palette {...seedColors[4]} />
     </div>
   );
 }
---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Palette extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+        {/* Navbar goes here */}
+        <div className='Palette-colors'>
+          {/* color boxes go here */}
+        </div>
+        {/* footer goes here */}
+      </div>
+    )
+  }
+}
+
+export default Palette;
