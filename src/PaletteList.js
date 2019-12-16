b7ae1adb5e9ccfceba101a0912ec22b104903bf7
Michael Clovis <michaelclovis@protonmail.com>
2019-12-16 03:31:20 -0800

    Finish styling MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -6,7 +6,7 @@ import { withStyles } from '@material-ui/styles';
 const styles = {
   root: {
     backgroundColor: 'blue',
-    height: '100%',
+    height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center'
@@ -16,8 +16,7 @@ const styles = {
     display: 'flex',
     alignItems: 'flex-start',
     flexDirection: 'column',
-    flexWrap: 'wrap',
-    border: '1px solid white'
+    flexWrap: 'wrap'
   },
   nav: {
     display: 'flex',
